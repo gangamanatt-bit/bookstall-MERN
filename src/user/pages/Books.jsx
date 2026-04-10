a3bebf28f415b76c402dbdaff73b83ed5cb14e00
gangamanatt-bit <gangamanatt@gmail.com>
2026-04-10 11:08:06 +0530

    bokks,view

--- src/user/pages/Books.jsx
+++ src/user/pages/Books.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from '../components/Header'
+import Footer from '../../components/Footer'
+import { FaBars } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 function Books() {
+  const [toggle,setToggle]=useState(false)
   return (
-    <div>
-      
+    <>
+    <Header/>
+    <>
+    <div className="flex flex-col justify-center items-center my-5">
+       <h1 className="text-3xl font-bold my-5">All Books</h1>
+       <div className="flex my-5">
+        <input type="text" className='p-2 border border-gray-200 w-100' placeholder='search by books title'/>
+        <button className="p-2 bg-blue-800 text-white">Search</button>
+       </div>
     </div>
+    </>
+    <div className="md:grid grid-cols-4 p-5 md:px-40 mb-10">
+      <div className="col-span-1">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">Filter</h1>
+          <button onClick={()=>setToggle(!toggle)} className="font-bold text-2xl md:hidden"><FaBars/></button>
+        </div>
+        <div className={toggle?'block':'hidden md:block'}>
+          <div className="mt-3">
+            <input type="radio" name='filter' id='no-filter' />
+            <label htmlFor="no-filter" className="ms-3">All</label>
+          </div>
+          {/* duplicate filter category */}
+           <div className="mt-3">
+            <input type="radio" name='filter' id='filter' />
+            <label htmlFor="filter" className="ms-3">Category</label>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-3">
+         <div className="md:grid grid-cols-4 w-full mt-5 md:mt-0">
+         <div className="shadow rounded p-3 m-4 md:mb-2">
+           <img width={'100%'} height={'300px'} src="https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-4.1.0&q=45&auto=format&w=1356&h=668&fit=crop" alt="book" />
+           <div className="flex flex-col justify-center items-center mt-4">
+             <h2 className="text-blue-700 font-bold text-xl">author</h2>
+             <h3 className="text-lg">title</h3>
+             <Link to={`/books/:id`} className="bg-blue-800 p-2 text-white mt-2">View books</Link>
+           </div>
+         </div>
+      </div>
+      </div>
+    </div>
+    <Footer/>
+      
+    </>
   )
 }
 
--- src/user/pages/Profile.jsx
+++ src/user/pages/Profile.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from '../components/Header'
+import Footer from '../../components/Footer'
+import { FaCircleCheck } from 'react-icons/fa6'
 
 function Profile() {
+  const [currentTab,setCurrentTab]=useState(1)
   return (
-    <div>
-      
-    </div>
+    <>
+    <Header/>
+      <div style={{height:"200px"}} className="bg-black"></div>
+      <div style={{width:'230px',height:'230px',borderRadius:'50%',marginTop:'-130px',marginLeft:'70px'}} className="bg-white p-3">
+        <img style={{width:'200px',height:'200px',borderRadius:'50%'}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLn2vN-qAufnhM8t2e4OkZ6-m3Md6_Gk9B7g&s" alt="user" />
+      </div>
+      <div className="md:flex justify-between px-20 mt-5">
+        <div className="flex items-center">
+          <h1 className="font-black md:text-3xl text-2xl">Username</h1>
+          <FaCircleCheck className='text-blue-400 ms-3'/>
+        </div>
+        edit
+      </div>
+      <p className="text-justify md:px-20 px-5 my-5">This is your personal space where you can manage your account, explore your reading activity, track your orders, and save books you love. Whether you're building a wishlist, discovering new arrivals, or reviewing your recent purchases, this page keeps everything organized and easy to access. Your reading journey continues here — enjoy exploring, discovering, and collecting stories that inspire you. ✨</p>  
+         <div className="md:px-40">
+          <div className="flex justify-center items-center my-8 font-medium text-lg">
+            <p onClick={()=>setCurrentTab(1)} className={currentTab==1?'p-4 border-grey-200 border-l border-t border-r rounded cursor-pointer':'p-4 border-grey-200 border-b rounded cursor-pointer'}>Upload book</p>
+            <p onClick={()=>setCurrentTab(2)} className={currentTab==2?'p-4 border-grey-200 border-l border-t border-r rounded cursor-pointer':'p-4 border-grey-200 border-b rounded cursor-pointer'}>Upload Book status</p>
+            <p onClick={()=>setCurrentTab(3)} className={currentTab==3?'p-4 border-grey-200 border-l border-t border-r rounded cursor-pointer':'p-4 border-grey-200 border-b rounded cursor-pointer'}>Purchase History</p>
+          </div>
+        {currentTab==1 &&  <div>Upload Book</div>}
+          {currentTab==2 && <div>Upload Book status</div>}
+        { currentTab==3 && <div>Purchase History</div>}
+         </div>
+          <Footer/>
+    </>
   )
 }
 
